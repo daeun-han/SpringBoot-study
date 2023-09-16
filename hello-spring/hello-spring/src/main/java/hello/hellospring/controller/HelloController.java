8feb95e39597415a0e8db557f1eb5b4d0e238291
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-09-16 15:46:23 +0900

    MVC와 템플릿 엔진
    섹션 2. 스프링 웹 개발 기초

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,21 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello") //url
     public String hello(Model model) {
-        model.addAttribute("data", "spring!!"); //data
+        model.addAttribute("data", "hello!!"); //data
         return "hello"; //template
     } // 이 메소드는 hello.html에 data 부분을 찾아서 hello!!값을 넣어주겠네
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name22") String name2, Model model) { //  hello-mvc?name22=spring! 하면, spring!이 name2 값으로 들어오고
+        model.addAttribute("name", name2); // 들어온 name2값이 template의 name 값으로 들어가는
+        return "hello-template";
+    }
 }
