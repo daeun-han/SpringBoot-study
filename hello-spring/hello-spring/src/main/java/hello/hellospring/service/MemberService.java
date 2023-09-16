731f138989f46aa3cc47e06fa121fff93a350129
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-09-16 19:03:11 +0900

    회원 서비스 테스트
    섹션 3. 회원 관리 예제 - 백엔드 개발

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,15 +2,18 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // 회원가입 서비스 로직
     public Long join(Member member) {
 
@@ -43,3 +46,5 @@ public class MemberService {
         return memberRepository.findById(memberId);
     }
 }
+
+// 단축키 ctrl shift t 사용해서 test 간편하게 만들기..
