46183f43ca4de6ccbd29d5394c9e91684b5eea0d
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-09-16 16:09:54 +0900

    API
    섹션 2. 스프링 웹 개발 기초

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name22") String name2, Model model) { //  hello-mvc?name22=spring! 하면, spring!이 name2 값으로 들어오고
         model.addAttribute("name", name2); // 들어온 name2값이 template의 name 값으로 들어가는
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //HTTP의 BODY에 직접 넣어주겠단 의미
+    public String helloString(@RequestParam("name22") String name) {
+        return "hello " + name; // 따라서 view(template)가 따로 필요 없음!
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 return
+        // @ResponseBody와 함께 객체를 return하게 되면 -> json으로 반환하게 됨
+    }
+    static class Hello { // static이용하면 class 안에 class 사용 가능
+        private String name;
+
+        // getter setter 단축키: ctrl + n -> all에서 getter setter 클릭
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
