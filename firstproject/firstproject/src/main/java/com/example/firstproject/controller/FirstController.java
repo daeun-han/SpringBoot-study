88bfe732b547b7adbb30f95940f3ec1339285bea
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-09-23 22:51:54 +0900

    2.3 MVC의 역할과 실행 흐름 이해하기

--- firstproject/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -13,4 +13,10 @@ public class FirstController {
         model.addAttribute("username", "daeun"); // username을 "다은"이라는 이름으로 추가
         return "greetings"; // greetings.mustache 파일 반환
     }
+
+    @GetMapping("/bye") // controller가 클라이언트의 요청을 받는 곳
+    public String seeYouNext(Model model) { // model 객체 생성 및 받아오기
+        model.addAttribute("nickname", "다은"); // 모델 변수 등록하기
+        return "goodbye";
+    }
 }
