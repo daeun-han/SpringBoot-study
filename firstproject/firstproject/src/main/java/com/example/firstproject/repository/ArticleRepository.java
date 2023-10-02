a6caa8f5a684aee8aa28cc31ee97db8ae8921c97
daeun han <daeunhan0723@gmail.com>
2023-10-02 19:33:20 +0900

    5.3 데이터 목록 조회하기

--- firstproject/firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,5 +3,10 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> { // 받는 제네릭 요소들 <관리 대상 엔티티의 클래스 타입, 엔티티의 대푯값(id) 타입>
+
+    @Override
+    ArrayList<Article> findAll(); // Iterable -> ArrayList 수정
 }
