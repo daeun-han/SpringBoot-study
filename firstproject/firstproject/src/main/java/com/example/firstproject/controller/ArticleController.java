4801efc2c2dcaf94991cb75c1cb0c902e3355c00
daeun han <daeunhan0723@gmail.com>
2023-10-02 19:00:10 +0900

    5.2 단일 데이터 조회하기

--- firstproject/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j // 로깅 기능을 위한 어노테이션
@@ -37,5 +39,20 @@ public class ArticleController {
 //        System.out.println(saved.toString()); // article이 DB에 잘 저장되는지 확인 출력
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model) { // 매개변수로 id 받아 오기, @PathVariable은 URL 요청으로 들어온 전달값을 컨트롤러의 매개변수로 가져오는 어노테이션이다.
+        log.info("id = "+id); // id 잘 받았는지 확인하는 로그
+
+//        1. id를 조회해 데이터 가져오기
+        // Optional 타입으로 반환 -> 반환형이 Article이 아님
+        Article articleEntity = articleRepository.findById(id).orElse(null); // id 값으로 데이터를 찾을 때, id 값이 없으면 null을 반환
+
+//        2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity); // article이라는 이름으로 articleEntity 객체 추가
+        
+//        3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
