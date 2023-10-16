956d23f6ef1c73839eb66fad95fa1aff300e26c5
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-10-16 16:47:25 +0900

    11.3.2 REST API: GET 구현하기

---
+++ firstproject/firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,32 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleApiController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    //GET
+    @GetMapping("/api/articles")
+    public List<Article> index() { // index 메서드 정의
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) { // show 메서드로 수정, url의 id를 매개변수로 받아 오기
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    //POST
+    //PATCH
+    //DELETE
+}
