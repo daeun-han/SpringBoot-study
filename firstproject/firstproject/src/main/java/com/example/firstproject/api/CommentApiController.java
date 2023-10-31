4e2f218927064ed1967f86054f68d2c7567d83a8
daeun-han <111632494+daeun-han@users.noreply.github.com>
2023-10-31 23:06:32 +0900

    15.3 댓글 조회하기

--- firstproject/firstproject/src/main/java/com/example/firstproject/api/CommentApiController.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -1,8 +1,13 @@
 package com.example.firstproject.api;
 
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CommentApiController {
@@ -10,6 +15,15 @@ public class CommentApiController {
     private CommentService commentService;
 
     // 1. 댓글 조회
+    @GetMapping("/api/articles/{articleId}/comments")
+    // 반환형이 ResponseEntity<List<CommentDto>>인 이유: DB에서 조회한 댓글 엔티티 목록은 List<Comment>이지만, 엔티티를 DTO로 변환하면 List<CommentDto>가 되기 때문에 + 응답코드를 같이 보내기 위해 ResponseEntity 클래스를 활용함
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        // 서비스에 위임
+        List<CommentDto> dtos = commentService.comments(articleId); // service의 메서드와 컨트롤러 메서드의 이름을 일부러 같게 지음
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
     // 2. 댓글 생성
     // 3. 댓글 수정
     // 4. 댓글 삭제
--- firstproject/firstproject/src/main/java/com/example/firstproject/service/CommentService.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,9 +1,15 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -11,4 +17,26 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private ArticleRepository articleRepository; // 게시글 리파지터리가 있어야 댓글을 생성할 때 대상 게시글의 존재 여부를 파악할 수 있음.
+
+    public List<CommentDto> comments(Long articleId) {
+
+//        // 1. 댓글 조회
+//        List<Comment> comments = commentRepository.findByArticleId(articleId); // List<Comment>타입인 이유: 한 게시글에 여러 댓글이 달릴 수 있기 때문 -> Comment를 리스트로 받아오기 위함
+//
+//        // 2. 엔티티 -> DTO 변환
+//        List<CommentDto> dtos = new ArrayList<CommentDto>(); //dtos는 레퍼런스 변수로 댓글 DTO를 관리하는 ArrayList를 가리킨다.
+//        for (int i = 0; i < comments.size(); i++) { // 조회한 댓글 엔티티 수만큼 반복하기
+//            Comment c = comments.get(i); // 하나씩 가져오기
+//            // 엔티티를 DTO로 변환
+//            CommentDto dto = CommentDto.createCommentDto(c); // createCommentDto는 댓글 엔티티를 입력받아 CommentDto 객체를 반환하는 메서드이다.
+//            dtos.add(dto); // 변환한 DTO를 dtos 리스트에 삽입
+//        }
+
+        // 서비스 코드의 for 문을 스트림 문법으로 개선
+        // 3. 결과 반환
+        return commentRepository.findByArticleId(articleId) // 댓글 엔티티 목록 조회
+                .stream() // 댓글 엔티티 목록을 스트림으로 변화
+                .map(comment -> CommentDto.createCommentDto(comment)) // 스트림의 각 요소(comment)를 꺼내 CommentDto.createCommentDto(comment)를 수행한 결과로 매핑 = 엔티티를 DTO로 매핑
+                .collect(Collectors.toList()); // 스트림 데이터를 리스트 자료형으로 변환
+    }
 }
